9f59c77a737d3d9805340d30969f2b223b8b3450
Ardilan <dilancorr@gmail.com>
2024-05-30 12:04:35 -0500

    ajustes finales terminado

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 window.onload = function () {
-  console.log("Hello World!");
+  document.getElementById("menuButton").addEventListener("click", function () {
+    var menu = document.getElementById("menu");
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+    } else {
+      menu.classList.add("hidden");
+    }
+  });
+
+  document.getElementById("menu").addEventListener("click", function () {
+    var menu = document.getElementById("menu");
+    menu.classList.add("hidden");
+  });
 };
